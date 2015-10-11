669dfcae0a5fe13555417955eb6970414b540443
Ken Hu <whosbacon@gmail.com>
2015-10-11 15:40:29 +0900

    add instruction and tips for 400

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ import reversedict
 
 app = flask.Flask(__name__)
 
+@app.route('/')
+def instruction():
+    return flask.jsonify(how_to='GET request to /lookup/{description}/ or POST to /lookup/ with parameter `description`.',
+                         tip='You can laso use `pos` argument to define part-of-speech of the intended word.')
+
 @app.route('/lookup/', methods=['GET','POST'])
 @app.route('/lookup/<description>/')
 def reversedict_lookup(description=None):
+    description = description or flask.request.form.get('description')
     if not description:
-        description = flask.request.form['description']
+        return flask.redirect(flask.url_for('instruction'))
     pos = flask.request.args.get('pos')
     results = reversedict.lookup(description, 
                                  pos=pos,
@@ -18,4 +24,7 @@ def reversedict_lookup(description=None):
     return flask.jsonify(suggestions=results)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT') or 5000))
+    app.run(host='0.0.0.0', 
+            debug=True,
+            port=int(os.environ.get('PORT') or 5000))
+    
