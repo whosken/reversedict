df58ab1bf91442ca98cdaad1f6abfab9221c2d0a
Ken Hu <whosbacon@gmail.com>
2016-02-24 15:55:22 +0900

    introduce elastic search

---
+++ reversedict/elastic.py
@@ -0,0 +1,17 @@
+import elasticsearch
+import os
+
+HOST = os.environ.get('SEARCHBOX_URL')
+
+class LazyClient(object):
+    def __init__(self):
+        self._client = False
+        
+    def __getattr__(self,name):
+        if name == '_client':
+            return self._client
+        if not self._client:
+            self._client = elasticsearch.Elasticsearch(HOST)
+        return getattr(self._client, name)
+    
+client = LazyClient()
