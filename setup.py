e3ccd10f2989fe1a27b39a426413f5823d1e52fe
Ken Hu <whosbacon@gmail.com>
2016-03-15 17:44:44 +0900

    bump pypi version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(name='reversedict',
-                 version='0.2',
+                 version='0.2.1',
                  description='Look up words by definition',
                  url='http://github.com/whosken/reversedict',
                  author='Ken Hu',
                  author_email='whosbacon@gmail.com',
                  license='MIT',
                  packages=['reversedict'],
-                 install_requires=['textblob'],
+                 install_requires=['textblob','elasticsearch'],
                  zip_safe=False)
