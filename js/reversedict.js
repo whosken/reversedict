ec55251adaafa9856900ee1077a42408b17980e2
Ken Hu <whosbacon@gmail.com>
2016-04-18 21:18:30 +0900

    attempts at jsonp

---
+++ js/reversedict.js
@@ -0,0 +1,30 @@
+ReverseDict = {
+    lookup:function(description, callback, handleParsingFailed){
+        $.ajax({
+            url:'http://reversedict.whosbacon.com/api/lookup/'+description,
+            dataType:'jsonp',
+            contentType:'application/json',
+            jsonpCallback:function(data){
+                if(!data.suggestions){
+                    console.log('Error parsing response', data);
+                    if(handleParsingFailed)
+                        return handleParsingFailed(data);
+                }
+                var parsedData = $.map(data.suggestions, ReverseDict.cleanData);
+                callback(parsedData);
+            },
+        });
+    },
+    
+    cleanData:function(data){
+        return {
+            term:ReverseDict.cleanTerm(data.term),
+            definitions:data.definitions,
+            synonyms:$.map(data.synonyms, ReverseDict.cleanTerm),
+        };
+    },
+    
+    cleanTerm:function(term){
+        return term.replace(/_/g,' ');
+    },
+};
