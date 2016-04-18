7c626e6ae0bccacf1d8542bb60851ddab91f7fb1
Ken Hu <whosbacon@gmail.com>
2016-04-18 21:22:16 +0900

    jsonp support

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import ujson as json
 
 import os
 
@@ -6,41 +7,23 @@ import reversedict
 
 app = flask.Flask(__name__)
 
-@app.route('/api/instructions')
+@app.route('/instructions')
 def instruction():
     return flask.jsonify(how_to='GET request to /lookup/{description}/ or POST to /lookup/ with parameter `description`.',
                          tip='You can also use `synonyms` parameter or `synonym` arguments to filter the candidates.')
 
-@app.route('/api/lookup/', methods=['GET','POST'])
-@app.route('/api/lookup/<description>/')
+@app.route('/lookup/', methods=['GET','POST'])
+@app.route('/lookup/<description>/')
 def reversedict_lookup(description=None):
     description = description or flask.request.form.get('description')
     if not description:
         return flask.redirect(flask.url_for('instruction'))
     synonyms = flask.request.form.get('synonyms') or flask.request.args.getlist('synonym')
     results = reversedict.lookup(description, synonyms)
+    if flask.request.args.get('callback'): # JSONP support
+        return '{}({});'.format(flask.request.args.get('callback'), 
+                                json.dumps(suggestions=results))
     return flask.jsonify(suggestions=results)
-
-
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
-
-@app.route('/js/<path:file_path>')
-def send_js(file_path):
-    return flask.send_from_directory('static/js', file_path)
-
-@app.route('/css/<path:file_path>')
-def send_css(file_path):
-    return flask.send_from_directory('static/css', file_path)
-
-@app.route('/img/<path:file_path>')
-def send_img(file_path):
-    return flask.send_from_directory('static/img', file_path)
-
-@app.route('/font-awesome/<path:file_path>')
-def send_font_awesome(file_path):
-    return flask.send_from_directory('static/font-awesome', file_path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', 
