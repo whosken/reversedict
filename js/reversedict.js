c6a6d75339a861a3d88ccd02e66e780ac3d4183c
Ken Hu <whosbacon@gmail.com>
2016-04-18 21:39:25 +0900

    fixing jsonp

--- js/reversedict.js
+++ js/reversedict.js
@@ -1,18 +1,20 @@
 ReverseDict = {
     lookup:function(description, callback, handleParsingFailed){
+        ReverseDict.jsonpCallback = function(data){
+            if(!data.suggestions){
+                console.log('Error parsing response', data);
+                if(handleParsingFailed)
+                    return handleParsingFailed(data);
+            }
+            var parsedData = $.map(data.suggestions, ReverseDict.cleanData);
+            callback(parsedData);
+        };
+        
         $.ajax({
-            url:'http://reversedict.whosbacon.com/api/lookup/'+description,
+            url:'http://reversedict.whosbacon.com/lookup/'+description,
             dataType:'jsonp',
             contentType:'application/json',
-            jsonpCallback:function(data){
-                if(!data.suggestions){
-                    console.log('Error parsing response', data);
-                    if(handleParsingFailed)
-                        return handleParsingFailed(data);
-                }
-                var parsedData = $.map(data.suggestions, ReverseDict.cleanData);
-                callback(parsedData);
-            },
+            jsonpCallback:'ReverseDict.jsonpCallback'
         });
     },
     
